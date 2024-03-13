56f6cc0dfcf80338268fa8ffc126a7e24dc0a6ce
ikakopa1985 <ikakopa1985@gmail.com>
2024-03-13 11:26:39 +0400

    added function calculate_with_sql

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sqlite3
 import random
+import time
 
 names = ['Chase Without Fear',
          'Child of The Plague',
@@ -50,13 +51,29 @@ c.execute(""" CREATE TABLE BOOKS(
 
 def calculate_with_python(data):
     print(data)
+    print()
+    print('calculate_with_python')
+    start_time = time.perf_counter()
     arr = [_[1] for _ in data]
     print(f'ყველა წიგნის გვერდების საშუალო რაოდენობა = {sum(arr) / len(data)}')
     max_account = None
     for item in data:
         if item[1] == max(arr):
             max_account = item
-    print(f'გვერდების მაქსიმალური რაოდენობის მქონე ჩანაწერია: {max_account}')
+
+    end_time = time.perf_counter()
+    print(f'გვერდების მაქსიმალური რაოდენობის მქონე ჩანაწერია: {max_account} \n time = {end_time - start_time}')
+
+
+def calculate_with_sql():
+    print()
+    print('calculate_with_sql')
+    start_time = time.perf_counter()
+    c.execute("SELECT avg(page_quantity) FROM BOOKS")
+    print(f'ყველა წიგნის გვერდების საშუალო რაოდენობა = {c.fetchall()[0][0]}')
+    c.execute("SELECT * FROM BOOKS ORDER BY page_quantity DESC ")
+    end_time = time.perf_counter()
+    print(f'გვერდების მაქსიმალური რაოდენობის მქონე ჩანაწერია: {c.fetchall()[0]} \n time = {end_time - start_time}')
 
 
 for _ in range(0, 10):
@@ -69,5 +86,5 @@ for _ in range(0, 10):
               (name_choice, page_quantity_choice, cover_type_choice, category_choice))
 
 c.execute("SELECT * FROM BOOKS")
-
 calculate_with_python(c.fetchall())
+calculate_with_sql()
